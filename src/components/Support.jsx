2a0a2ae6c1d202ef2f2064976a28bdd9b730f703
Syed Faiq Haider Naqvi <syedfaiqhaider126@gmail.com>
2025-05-14 16:55:21 +0500

    Github Repo link added.

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -21,15 +21,15 @@ function Support() {
         <div className='flex items-center justify-between gap-4 py-8 px-4 sm:px-6 lg:px-8'>
           <div className='flex flex-col items-center gap-2 m-2 ml-5 md:ml-10'>
               <img className="md:w-18 md:h-18 w-15 h-15" src={maleprofile_svg} alt="" />
+              <p className='font-semibold md:text-lg'>2022609</p>
+          </div>
+          <div className='flex flex-col items-center gap-2 m-2'>
+              <img className="md:w-18 md:h-18 w-15 h-15" src={maleprofile_svg} alt="" />
               <p className='font-semibold md:text-lg'>2022562</p>
           </div>
           <div className='flex flex-col items-center gap-2 m-2'>
               <img className="md:w-18 md:h-18 w-15 h-15" src={maleprofile_svg} alt="" />
               <p className='font-semibold md:text-lg'>2022211</p>
-          </div>
-          <div className='flex flex-col items-center gap-2 m-2'>
-              <img className="md:w-18 md:h-18 w-15 h-15" src={maleprofile_svg} alt="" />
-              <p className='font-semibold md:text-lg'>2022609</p>
           </div>
           <div className='flex flex-col items-center gap-2 m-2 mr-5 md:mr-10'>
               <img className="md:w-18 md:h-18 w-15 h-15" src={maleprofile_svg} alt="" />
